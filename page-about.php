3a0c68c02d0cc92d6e4ceaac136d4d6bf2af39a8
John Simpson <johnathan.l.simpson@gmail.com>
2019-05-30 11:22:59 -0400

    Revised copy/styles on 'about' page, slightly revised image placement on 'design' page.

--- page-about.php
+++ page-about.php
@@ -3,16 +3,71 @@
 		About
 	</h1>
 	
+	<section class="text--primary">
+		<div>
+			<h2>
+				Hi, I'm <span>John</span>, a frontend web developer and designer from Harrisonburg, VA.
+			</h2>
+			<p>
+				I 💕 PHP, SQL, CSS, and <a href="https://last.fm/user/inartistic/" target="_blank">bad music</a>. Need a website? <a href="mailto:johnathan.l.simpson@gmail.com" target="_blank">Let's talk</a>. Or, find me below.
+			</p>
+			
+			<ul>
+				<li>Review my code on <a href="https://github.com/johnathanlsimpson/" target="_blank">Github</a>.</li>
+				<li>Read about my projects on <a href="https://medium.com/@johnathan.l.simpson" target="_blank">Medium</a>.</li>
+				<li>Get my bad opinions on <a href="https://twitter.com/vkgy_" target="_blank">Twitter</a>.</li>
+				<li>Check out my cats on <a href="https://www.instagram.com/iinartistic/" target="_blank">Instagram</a>.</li>
+			</ul>
+		</div>
+	</section>
+	
+	<style>
+		.section--about section {
+			align-items: center;
+			bottom: 0;
+			color: rgb(var(--yellow));
+			display: flex;
+			font-size: 1.1rem;
+			font-size: 1.25rem;
+			font-weight: bold;
+			justify-content: center;
+			left: 0;
+			letter-spacing: 2px;
+			line-height: 1.5em;
+			padding: 4rem;
+			position: absolute;
+			right: 0;
+			text-transform: none;
+			top: 0;
+		}
+		.section--about section > div {
+			max-width: 100%;
+			width: 500px;
+		}
+		.section--about h2 {
+			color: rgb(var(--purple));
+			font-size: 1.25em;
+			line-height: 1.75em;
+		}
+		.section--about .text--primary span {
+			font-family: 'pirata one';
+			font-size: 2em;
+			font-weight: normal;
+			letter-spacing: 1px;
+			vertical-align: text-bottom;
+		}
+		.section--about section div > * + * {
+			margin-top: 1.75em;
+		}
+	</style>
 	
 	
 	
 	
 	
-	<section class="work__container work__details middled" style="height: 100%;">
+	
+	<!--<section class="work__container work__details middled" style="height: 100%;">
 		
-		<!--<span class="work__title text--spaced design__title" href="https://vk.gy/" style="color:black; background:white;">
-			Desig<l>n</l>
-		</span>-->
 		
 		<div class="work__summary" style="color: rgb(var(--yellow)); border:none; width: 100%; padding: 0; align-items: center;">
 			
@@ -50,7 +105,7 @@
 		</style>
 		
 		
-	</section>
+	</section>-->
 	
 	
 	
--- page-design.php
+++ page-design.php
@@ -6,71 +6,39 @@
 	
 	
 	
-	<!-- vk.gy -->
 	<section class="work__container work__container--top work__images design__images-wrapper">
+		<div class="work__images-container design__images-container design__images--top" style="border:none;">
+			
+			<div class="work__image" style="background-image: url(images/design-book.jpg); grid-column: 1 / span 5; grid-row: 1 / span 6; background-size:contain; background-position: bottom;" data-caption="Book jacket"></div>
+			<div class="work__image" style="background-image: url(images/design-postcard_christmas.png); grid-column: 13 / span 4; grid-row: 1 / span 4;" data-caption="Record label Christmas card"></div>
+			<div class="work__image" style="background-image: url(images/design-logo_chikasen.png); grid-column: 12 / span 4; grid-row: 6 / span 4; background-size:contain;"></div>
+			<div class="work__image" style="background-image: url(images/design-sticker-2.png); grid-column: 17 / span 4; grid-row: 6 / span 4; background-size: contain;"></div>
+			<div class="work__image" style="background-image: url(images/design-sticker-1.png); grid-column: 17 / span 4; grid-row: 1 / span 4; background-size: contain;"></div>
+			<div class="work__image" style="background-image: url(images/design-logo_counseling.png); grid-column: 7 / span 6; grid-row: 1 / span 3;background-size:contain;"></div>
+			<div class="work__image" style="background-image: url(images/design-logo_aurea.png); grid-column: 1 / span 5; grid-row: 8 / span 2; background-size:contain;"></div>
+			<div class="work__image" style="background-image: url(images/design-logo_vkgy.png); grid-column: 7 / span 4; grid-row: 4 / span 6;background-size: contain;"></div>
 		
-<div class="work__images-container design__images-container design__images--top" style="border:none;">
-<!--<div class="work__image work__image--bottom work__image--horizontal" style="background-image: url(/images/design-poster.jpg); grid-column: 1 / span 9; grid-row: 3 / span 7;"></div>-->
-
-	
-	
-<div class="vkgy--2 work__image"     style="background-image:url(/images/design-book.jpg); grid-column: 1 / span 5; grid-row: 1 / span 6; background-size:contain; background-position: bottom;" data-caption="Book jacket"></div>
-	
-<div class="work__image" style="background-image: url(/images/design-postcard_christmas.png); grid-column: 13 / span 4; grid-row: 1 / span 4;" data-caption="Record label Christmas card"></div>
-<div class="work__image" style="background-image: url(/images/design-logo_chikasen.png); grid-column: 12 / span 4; grid-row: 6 / span 4; background-size:contain;"></div>
-	
-<div class="work__image" style="background-image: url(/images/design-sticker-2.png); grid-column: 17 / span 4; grid-row: 6 / span 4; background-size: contain;"></div>
-<div class="work__image" style="background-image: url(/images/design-sticker-1.png); grid-column: 17 / span 4; grid-row: 1 / span 4; background-size: contain;"></div>
-	
-	
-<div class="work__image" style="background-image: url(/images/design-logo_counseling.png); grid-column: 7 / span 6; grid-row: 1 / span 3;background-size:contain;"></div>
-<div class="work__image" style="background-image: url(/images/design-logo_aurea.png); grid-column: 1 / span 5; grid-row: 8 / span 2; background-size:contain;"></div>
-
-	
-<div class="work__image" style="background-image: url(/images/design-logo_vkgy.png); grid-column: 7 / span 4; grid-row: 4 / span 6;background-size: contain;"></div>
-
-
-</div>
+		</div>
 	</section>
 
 	
 	
 	
-	
-	<!-- vk.gy -->
 	<section class="work__container work__container--bottom work__images design__images-wrapper">
-		
 		<div class="work__images-container design__images-container design__images--bottom">
-
 			
+			<div class="work__image work__image--horizontal" style="background-image: url(images/design-shirt_lamina.png); grid-column: 4 / span 5; grid-row: 1 / span 5;"></div>
+			<div class="work__image work__image--horizontal" style="background-image: url(images/design-shirt_dali.png); grid-column: 8 / span 5; grid-row: 1 / span 5;"></div>
+			<div class="work__image work__image--horizontal" style="background-image: url(images/design-shirt_atama.png); grid-column: 12 / span 5; grid-row: 1 / span 5;"></div>
+			<div class="work__image work__image--horizontal" style="background-image: url(images/design-shirt_lily.png); grid-column: 16 / span 5; grid-row: 1 / span 5;"></div>
+			<div class="work__image" style="background-image:url(images/design-postcard.jpg); grid-column: 7 / span 4; grid-row: 6 / span 5;"></div>
+			<div class="work__image" style="background-image: url(images/design-avatars.png); grid-column: 14 / span 7; grid-row: 5 / span 6; background-size: contain;"></div>
+			<div class="work__image" style="background-image: url(images/design-logo_proposal.png); grid-column: 1 / span 3; grid-row: 2 / span 3; background-size: contain;"></div>
+			<div class="work__image" style="background-image: url(images/design-symbol_butterfly.png); grid-column: 12 / span 2; grid-row: 5 / span 2; background-position: bottom -1rem left -1rem;"></div>
+			<div class="work__image" style="background-image: url(images/design-symbol_clock.png); grid-column: 12 / span 2; grid-row: 7 / span 2; background-position: bottom -1rem left -1rem;"></div>
+			<div class="work__image" style="background-image: url(images/design-symbol_moon.png); grid-column: 12 / span 2; grid-row: 9 / span 2; background-position: bottom -1rem left -1rem;"></div>
+			<div class="work__image" style="background-image: url(images/design-logo_a.png); grid-column: 1 / span 5; grid-row: 7 / span 4;background-size:contain;"></div>
 			
-<div class="work__image work__image--horizontal" style="background-image: url(/images/design-shirt_lamina.png); grid-column: 4 / span 5; grid-row: 1 / span 5;"></div>
-<div class="work__image work__image--horizontal" style="background-image: url(/images/design-shirt_dali.png); grid-column: 8 / span 5; grid-row: 1 / span 5;"></div>
-<div class="work__image work__image--horizontal" style="background-image: url(/images/design-shirt_atama.png); grid-column: 12 / span 5; grid-row: 1 / span 5;"></div>
-<div class="work__image work__image--horizontal" style="background-image: url(/images/design-shirt_lily.png); grid-column: 16 / span 5; grid-row: 1 / span 5;"></div>
-
-			
-			
-<div class="vkgy--3 work__image" style="background-image:url(/images/design-postcard.jpg); grid-column: 7 / span 4; grid-row: 6 / span 5;"></div>
-			
-		
-		
-<div class="work__image" style="background-image: url(/images/design-avatars.png); grid-column: 14 / span 7; grid-row: 5 / span 6; background-size: contain;"></div>
-			
-			
-			
-<div class="work__image" style="background-image: url(/images/design-logo_proposal.png); grid-column: 1 / span 3; grid-row: 2 / span 3; background-size: contain;"></div>
-			
-			
-<div class="work__image" style="background-image: url(/images/design-symbol_butterfly.png); grid-column: 12 / span 2; grid-row: 5 / span 2;"></div>
-<div class="work__image" style="background-image: url(/images/design-symbol_clock.png); grid-column: 12 / span 2; grid-row: 7 / span 2;"></div>
-<div class="work__image" style="background-image: url(/images/design-symbol_moon.png); grid-column: 12 / span 2; grid-row: 9 / span 2;"></div>
-
-<div class="work__image" style="background-image: url(/images/design-logo_a.png); grid-column: 1 / span 5; grid-row: 7 / span 4;background-size:contain;"></div>
-	
-		
-		
-		
 		</div>
 	</section>
 	
@@ -79,7 +47,7 @@
 	
 	<section class="work__container work__details middled" style="height: 100%;">
 		
-		<span class="work__title text--spaced design__title" href="https://vk.gy/" style="color:black; background:white;">
+		<span class="work__title text--spaced" >
 			Desig<l>n</l>
 		</span>
 		
@@ -93,21 +61,6 @@
 		
 		<a class="a--circled a--filled text--primary text--spaced" href="https://medium.com/@johnathan.l.simpson/vk-gy-fc1376fd0b4c" target="_blank">Rea<l>d</l></a>
 	</section>
-
-	<!--<section class="work__container work__container--bottom work__details middled" style="height: 50%; bottom:0;">
-		
-		<span class="work__title text--spaced design__title" href="https://vk.gy/" style="background: white; color: black;">
-			Digita<l>l</l>
-		</span>
-		
-		<div class="work__summary work__summary--right">
-			<p class="work__description design__description text--secondary">
-				<span class="work__description-bg">Inclusive, customisable avatar system for online community.</span>
-				<span class="work__tech">Illustrator</span>
-				<span class="work__tech">Photoshop</span>
-			</p>
-		</div>
-	</section>-->
 	
 	
 	
