255ed0248d3597ad576dd44e83c9d9cdb5c23cde
John Simpson <johnathan.l.simpson@gmail.com>
2019-06-26 15:41:29 -0400

    [Change] Joined images and resized/compressed.

--- page-design.php
+++ page-design.php
@@ -8,16 +8,7 @@
 	
 	<section class="work__container work__container--top work__images design__images-wrapper">
 		<div class="work__images-container design__images-container design__images--top" style="border:none;">
-			
-			<div class="work__image" style="background-image: url(images/design-book.jpg); grid-column: 1 / span 5; grid-row: 1 / span 6; background-size:contain; background-position: bottom;" data-caption="Book jacket"></div>
-			<div class="work__image" style="background-image: url(images/design-postcard_christmas.png); grid-column: 13 / span 4; grid-row: 1 / span 4;" data-caption="Record label Christmas card"></div>
-			<div class="work__image" style="background-image: url(images/design-logo_chikasen.png); grid-column: 12 / span 4; grid-row: 6 / span 4; background-size:contain;"></div>
-			<div class="work__image" style="background-image: url(images/design-sticker-2.png); grid-column: 17 / span 4; grid-row: 6 / span 4; background-size: contain;"></div>
-			<div class="work__image" style="background-image: url(images/design-sticker-1.png); grid-column: 17 / span 4; grid-row: 1 / span 4; background-size: contain;"></div>
-			<div class="work__image" style="background-image: url(images/design-logo_counseling.png); grid-column: 7 / span 6; grid-row: 1 / span 3;background-size:contain;"></div>
-			<div class="work__image" style="background-image: url(images/design-logo_aurea.png); grid-column: 1 / span 5; grid-row: 8 / span 2; background-size:contain;"></div>
-			<div class="work__image" style="background-image: url(images/design-logo_vkgy.png); grid-column: 7 / span 4; grid-row: 4 / span 6;background-size: contain;"></div>
-		
+			<div class="work__image" style="background-image: url(img-design-top.png); grid-column: 1 / span 20; grid-row: 1 / span 20;"></div>
 		</div>
 	</section>
 
@@ -26,19 +17,7 @@
 	
 	<section class="work__container work__container--bottom work__images design__images-wrapper">
 		<div class="work__images-container design__images-container design__images--bottom">
-			
-			<div class="work__image work__image--horizontal" style="background-image: url(images/design-shirt_lamina.png); grid-column: 4 / span 5; grid-row: 1 / span 5;"></div>
-			<div class="work__image work__image--horizontal" style="background-image: url(images/design-shirt_dali.png); grid-column: 8 / span 5; grid-row: 1 / span 5;"></div>
-			<div class="work__image work__image--horizontal" style="background-image: url(images/design-shirt_atama.png); grid-column: 12 / span 5; grid-row: 1 / span 5;"></div>
-			<div class="work__image work__image--horizontal" style="background-image: url(images/design-shirt_lily.png); grid-column: 16 / span 5; grid-row: 1 / span 5;"></div>
-			<div class="work__image" style="background-image:url(images/design-postcard.jpg); grid-column: 7 / span 4; grid-row: 6 / span 5;"></div>
-			<div class="work__image" style="background-image: url(images/design-avatars.png); grid-column: 14 / span 7; grid-row: 5 / span 6; background-size: contain;"></div>
-			<div class="work__image" style="background-image: url(images/design-logo_proposal.png); grid-column: 1 / span 3; grid-row: 2 / span 3; background-size: contain;"></div>
-			<div class="work__image" style="background-image: url(images/design-symbol_butterfly.png); grid-column: 12 / span 2; grid-row: 5 / span 2; background-position: bottom -1rem left -1rem;"></div>
-			<div class="work__image" style="background-image: url(images/design-symbol_clock.png); grid-column: 12 / span 2; grid-row: 7 / span 2; background-position: bottom -1rem left -1rem;"></div>
-			<div class="work__image" style="background-image: url(images/design-symbol_moon.png); grid-column: 12 / span 2; grid-row: 9 / span 2; background-position: bottom -1rem left -1rem;"></div>
-			<div class="work__image" style="background-image: url(images/design-logo_a.png); grid-column: 1 / span 5; grid-row: 7 / span 4;background-size:contain;"></div>
-			
+			<div class="work__image" style="background-image: url(img-design-bottom.png); grid-column: 1 / span 20; grid-row: 1 / span 20;"></div>
 		</div>
 	</section>
 	
